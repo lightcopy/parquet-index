2b54cfb7bba42aed3e96f2c2b3473aa30ff2078c
Ivan Sadikov <isadikov@wynyardgroup.com>
2016-12-02 12:55:31 +1300

    add parquet schema conversion

--- src/main/scala/com/github/lightcopy/index/parquet/ParquetSource.scala
+++ src/main/scala/com/github/lightcopy/index/parquet/ParquetSource.scala
@@ -16,7 +16,23 @@
 
 package com.github.lightcopy.index.parquet
 
+import java.util.Arrays
+
+import scala.collection.JavaConverters._
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.FileStatus
+
+import org.apache.parquet.hadoop.ParquetFileReader
+import org.apache.parquet.hadoop.metadata.ParquetMetadata
+
+import org.apache.parquet.schema.{GroupType, MessageType, OriginalType, PrimitiveType, Type}
+import org.apache.parquet.schema.OriginalType._
+import org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName._
+import org.apache.parquet.schema.Type.Repetition.{OPTIONAL, REPEATED, REQUIRED}
+
 import org.apache.spark.sql.Column
+import org.apache.spark.sql.types._
 
 import com.github.lightcopy.{Catalog, IndexSpec}
 import com.github.lightcopy.index.{Index, IndexSource, Metadata}
@@ -28,5 +44,92 @@ class ParquetSource extends IndexSource {
 
   override def createIndex(catalog: Catalog, spec: IndexSpec, columns: Seq[Column]): Index = {
     throw new UnsupportedOperationException()
+  }
+
+  /** Retrieve footer metadata from provided status, expected only one footer */
+  private def footerMetadata(conf: Configuration, status: FileStatus): ParquetMetadata = {
+    val footers = ParquetFileReader.readAllFootersInParallelUsingSummaryFiles(conf,
+      Arrays.asList(status), false)
+    footers.get(0).getParquetMetadata
+  }
+
+  /** Convert column into column name, should work for nested types */
+  private def withColumnName(column: Column): String = column.toString
+
+  /**
+   * Convert Parquet schema into StructType of columns supported by index, it does not convert
+   * entire message type. Also note that only certain Parquet types are supported for index.
+   * Do not check on MatchError
+   */
+  private def convert(parquetSchema: MessageType, colNames: Seq[String]): StructType = {
+    val fieldsSet = colNames.toSet
+    val fields = parquetSchema.getFields.asScala.flatMap { field =>
+      if (fieldsSet.contains(field.getName)) {
+        val structField = field.getRepetition match {
+          case OPTIONAL => StructField(field.getName, convertField(field), nullable = true)
+          case REQUIRED => StructField(field.getName, convertField(field), nullable = false)
+          case REPEATED => throw new UnsupportedOperationException(
+            "Parquet repeated fields are not supported")
+        }
+        Some(structField)
+      } else {
+        None
+      }
+    }
+
+    if (fields.length != colNames.length) {
+      throw new IllegalStateException(
+        s"Schema mismatch, requested columns ${colNames.mkString("[", ", ", "]")} are different " +
+        s"from extracted fields ${fields.mkString("[", ", ", "]")} for schema $parquetSchema")
+    }
+
+    StructType(fields)
+  }
+
+  /** Convert individual field into `StructField`, do not check on MatchError */
+  private def convertField(parquetType: Type): DataType = parquetType match {
+    case primitive: PrimitiveType => convertPrimitiveField(primitive)
+    case group: GroupType => convertGroupField(group)
+  }
+
+  /** Convert field into StructField of primitive type */
+  private def convertPrimitiveField(tpe: PrimitiveType): DataType = {
+    val originalType = tpe.getOriginalType
+    val typeName = tpe.getPrimitiveTypeName
+    typeName match {
+      case BOOLEAN => BooleanType
+      case FLOAT => FloatType
+      case DOUBLE => DoubleType
+      case INT32 =>
+        originalType match {
+          case INT_8 => ByteType
+          case INT_16 => ShortType
+          case INT_32 | null => IntegerType
+          case other => typeNotSupported(other)
+        }
+      case INT64 =>
+        originalType match {
+          case INT_64 | null => LongType
+          case other => typeNotSupported(other)
+        }
+      case BINARY =>
+        originalType match {
+          case UTF8 | ENUM | JSON => StringType
+          case other => typeNotSupported(other)
+        }
+      case otherTypeName =>
+        throw new UnsupportedOperationException(
+          s"Parquet primitive type $otherTypeName is not supported")
+    }
+  }
+
+  /** Convert field into StructField of group type */
+  private def convertGroupField(field: GroupType): DataType = {
+    throw new UnsupportedOperationException(s"Parquet index does not support group type $field")
+  }
+
+  /** Type not support error */
+  private def typeNotSupported(tpe: OriginalType): DataType = {
+    throw new UnsupportedOperationException(s"Parquet original type $tpe is not supported")
   }
 }
